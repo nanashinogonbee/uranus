ddc46d0649e29cf8ccf752dbc27b9df24dfeb624
nanashinogonbee <vgpro80@gmail.com>
2019-03-04 01:10:00 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # uranus
-not funny anymore
+An extension that replaces _Uranus_ with _Your anus_.
+
+---
+
+__Examples:__
+
+![Example 1](examples/example1.png "Example 1")
+
+![Example 2](examples/example2.png "Example 2")
+
+---
+
+To install, just drag and drop the `uranus.crx` file into `chrome://extensions` tab and click the "Add extension" button.
+
+![Adding an extension, step 1](examples/ext_add1.png "Adding an extension, step 1")
+
+![Adding an extension, step 2](examples/ext_add2.png "Adding an extension, step 2")
---
+++ src/index.js
@@ -0,0 +1,18 @@
+let elements = document.getElementsByTagName('*');
+
+for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+
+    for (let j = 0; j < element.childNodes.length; j++) {
+        let node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            let text = node.nodeValue;
+            let replacedText = text.replace(/Uranus/gi, 'Your anus');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
